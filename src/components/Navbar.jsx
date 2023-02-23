95002041a5e907d3fd866794c9e867c97c56b835
joe-leahy <josleahy@icloud.com>
2023-02-23 12:28:39 -0500

    initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <Link href='/'>
+            <h1>JLC</h1>
+            </Link>
+            
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
