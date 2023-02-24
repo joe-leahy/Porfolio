09142f36a4cc4cddca8f1e2ac296bcf292e1f667
joe-leahy <josleahy@icloud.com>
2023-02-24 12:46:06 -0500

    temp <img>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import {Cursor, useTypewriter} from 'react-simple-typewriter'
-import BackgroundCircles from './BackgroundCircles'
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Image from 'next/image'
 
 const Hero = () => {
-
-    const[text, count] = useTypewriter({
-        words:[
-            "Photographer, Engineer, Explorer",
-            "Full Stack Software Developer",
-            "Hi, I'm Joe"
-        ],
-        loop:true,
-        delaySpeed: 1200
-    })
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, I'm Joe",
+      "Photographer",
+      "Engineer",
+      "<FrontendDeveloper />",
+    ],
+    loop: true,
+    delaySpeed: 1200,
+  });
 
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
-        <BackgroundCircles />
-        
-        <h1>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://avatars.githubusercontent.com/u/112727413?v=4"
+        alt="profilepic"
+      />
+      <h1>
         <span>{text}</span>
-        <Cursor cursorColor='blue'/>
-        </h1>
+        <Cursor cursorColor="blue" />
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
