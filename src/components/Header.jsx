63ff7a6dd0fd7a04283fbddb5906122bc2135c52
joe-leahy <josleahy@icloud.com>
2023-02-24 15:43:59 -0500

    About me

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+    <header className="sticky top-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div 
         initial={{
             x:-500,
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,11 +7,9 @@ import Link from "next/link";
 const Hero = () => {
   const [text, count] = useTypewriter({
     words: [
-      "Fullstack_Developer.js",
-      "<FrontendSpecialist />",
       "Photographer",
-      "UI_Designer.jsx",
-
+      "Software Engineer",
+      "UI Designer",
     ],
     loop: true,
     delaySpeed: 1200,
@@ -27,7 +25,7 @@ const Hero = () => {
       />
 
       <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Ride | Explore | Create</h2>
       <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
         <span className="mr-3">{text}</span>
         <Cursor cursorColor="blue" />
